da3cf006b1f8a03c20bf4d912f87318e3e8e2672
Jennifer Zhu <jenniferzhu@umass.edu>
2016-02-06 15:05:36 -0500

    Added the html and css files for creating an account page, but still need to get it working properly.

--- main.py
+++ main.py
@@ -15,11 +15,38 @@
 # limitations under the License.
 #
 import webapp2
+import logging
+import os
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment (
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True
+)
+
+class CreateAccountHandler(webapp2.RequestHandler):
+    def get(self):
+        header_template = JINJA_ENVIRONMENT.get_template('templates/header.html')
+        header_values = {}
+        header_values["page_title"] = "match.me | Account Settings"
+        header_values["link_to_stylesheet"] = "css/create-account.css"
+        header_values["script_source"] = "http://yui.yahooapis.com/3.18.1/build/yui/yui-min.js"
+        # header_values["link_to_stylesheet"] = "../css/header.css"
+        self.response.write(header_template.render(header_values))
+
+        # body_template = JINJA_ENVIRONMENT.get_template('templates/create-account.html')
+        create_account_page = open('templates/create-account.html').read()
+        self.response.write(create_account_page)
+
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/create-account', CreateAccountHandler),
 ], debug=True)
